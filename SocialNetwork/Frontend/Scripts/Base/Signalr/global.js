9c9da8d38b33367e41aeb324621f26a5f9ee3754
mathewjames-dev <enquiries@mathewjames.co.uk>
2020-07-24 22:30:28 +0100

    Updates to styling and also the javascript structuring and code.
    Started to refactor the view javascript into vue.

---
+++ SocialNetwork/Frontend/Scripts/Base/Signalr/global.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/global")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.start().then(
+    () => console.log("connected")
+).catch(
+    err => console.error(err)
+);  
+
+connection.on("BroadcastMessage", function (user, message) {
+    console.log('received');
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    alert(encodedMsg);
+});
--- SocialNetwork/Frontend/Scripts/site.js
+++ SocialNetwork/Frontend/Scripts/site.js
@@ -1,3 +1,42 @@
-﻿const app = new Vue({
-    el: '#app'
+﻿// Vendor Files
+import Vue from 'vue';
+import Axios from 'axios';
+
+// Base Files
+import SignalRGlobal from './Base/Signalr/global';
+
+// Home Page Vue
+const Home = new Vue({
+    el: '#home',
+    // define methods under the `methods` object
+    methods: {
+        likeStatus: function (event) {
+            var data = {
+                userId: event.target.parentElement.querySelector('input[name="userId"]').value,
+                postId: event.target.parentElement.querySelector('input[name="postId"]').value
+            }
+
+            if (event.target.classList.contains('post-liked')) {
+                // Post has already been liked.
+                data.liked = 1;
+                data.url = 'UnlikePost';
+            } else if (event.target.classList.contains('post-unliked')) {
+                // Post hasn't already been liked.
+                data.liked = 0;
+                data.url = 'LikePost';
+            }
+
+            axios.post('/Ajax/' + data.url, data)
+                .then(function (r) {
+                    console.log(r);
+                    if (data.response == "Success") {
+                        event.target.removeClass('post-unliked').addClass('post-liked');
+                        postLikesHtml($this, data);
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
 });
